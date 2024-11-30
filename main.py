e0168d04fac714b28e7fececd82b6dc76e797724
DawoodKhatri <dawoodkhatri18@gmail.com>
2024-11-30 19:42:58 +0530

    Added Cors

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from chat import handle_chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QueryModel(BaseModel):
     message: str
